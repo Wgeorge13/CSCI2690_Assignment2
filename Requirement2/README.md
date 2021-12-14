940f254d68727ce8279ee6d40e5eb69095d6b063
zhihaochen <zhihaochen0329@gmail.com>
2021-12-14 02:57:20 -0400

    zhihaochen-capstone project question 1

--- Requirement2/README.md
+++
@@ -1,4 +0,0 @@
-# CSCI2690_Assignment2
-Assignment2 Requirement2 
-
-body performance table structure
--- Requirement3/psql/initScripts/init.sql
+++
@@ -1,9 +0,0 @@
-create database testdb;
-
-\c testdb
-
-create table testTable(id int,title varchar(100),primary key(id));
-
-insert into testTable values(1,'test title1');
-
-insert into testTable values(2,'test title2');
